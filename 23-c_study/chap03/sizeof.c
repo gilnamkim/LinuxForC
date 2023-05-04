972ed45617ef98c05835a1b200d1704b30da5af2
gilnamkim <dustin0627@naver.com>
2023-05-04 12:01:00 +0900

    변수와 상수

---
+++ 23-c_study/chap03/sizeof.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	
+	int a;
+	a = 8;
+
+	printf("a의 값 %d size : %d\n", a, sizeof(a));
+	// 임베디드를 포함한 다양한 시스템에 따라 변하는 int외 다양한 형의 
+	// 크가를 알고 싶을 때 sizeof라는 함수를 사용합니다.
+	
+	// const int a = 3;  a=3; << 못바꿈
+	//
+	// #define a 3;  전처리기가 3으로 바꾸고 컴파일
+	
+
+	return 0;}
---
+++ 23-c_study/chap03/sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+	int a = 2 + 3;
+	int b = 2 - 1;
+
+	int c = 3;
+	int d = 4;
+
+	int f = c + d;
+
+	printf("%d\n",a);
+	printf("%d\n",b);
+	printf("%d\n",c);
+	printf("%d\n",d);
+	printf("%d\n",f);
+
+
+
+	return 0;
+}
