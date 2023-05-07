0ab6d015c081b07e039ed4f528929319f18fc0e3
gilnamkim <dustin0627@naver.com>
2023-05-07 16:37:52 +0900

    함수

---
+++ 23-c_study/chap08/function01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int function1(int a, int b) {
+	return a+b;
+}
+
+int function2(int a, int b, int c) {
+	return a-b-c;
+}
+
+int main() {
+	
+	printf("%d\n",function1(1,2));
+
+	printf("%d\n",function2(10,4,1));
+
+	return 0;
+}
