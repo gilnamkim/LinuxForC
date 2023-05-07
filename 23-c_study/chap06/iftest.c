f9c14ed84870d905fb7c80660034fcd077c91b1c
gilnamkim <dustin0627@naver.com>
2023-05-07 15:44:11 +0900

    if조건문

--- 23-c_study/chap06/iftest.c
+++ 23-c_study/chap06/iftest.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 
+int isGood();
+int isBad();
+
 int main() {
 	
-	int a;
-	
-	printf("정수를 입력 : __\b\b");
-	scanf("%d",&a);
+	// c언어에서 참거짓을 구분하는것은
+	// 참은 0이 아니면 모두 참
+	// 0이면 거짓
+	//
+	// 참일 확률이 가장 높아보이는 것을 가장 상위에 두는것이 보기 좋다
 
-	printf("입력 값 : %d\n",a);
+	int a = 0;
+
+	if(isGood()){           // 리턴값이 1이므로 if문 내부를 실행한다
+		a = 1;
+	}
+
+	if(isBad()){            // 리턴값이 0이므로 if문 내부를 실행하지 않는다
+		a = 2;
+	}
+	
+	
+	printf("결과: %d\n", a);
 
 	return 0;
 
 
 }
+
+int isGood(){
+	return 1;
+}
+
+int isBad() {
+	return 0;
+}
+
