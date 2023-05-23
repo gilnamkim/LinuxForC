1aab6feaed9a0fd65e6f2b5e428a1b4971707551
gilnamkim <dustin0627@naver.com>
2023-05-23 20:38:25 +0900

    linkedList구현

--- 23-c_study/chap15/linkedList.c
+++ 23-c_study/chap15/linkedList.c
@@ -12,7 +12,27 @@ typedef struct data {
 data * g_head = NULL;
 data * g_tail = NULL;
 
+data *find(int num) {
+	if(g_head == NULL) {
+		printf("no data...\n");
+		return NULL;
+	}
+	data *temp = g_head;
+	while(temp){
+		if(temp->num == num){
+			return temp;
+		}
+		temp = temp->next;
+	}
+	return NULL;
+}
+
+
 void printAll() {
+	if(g_head == NULL) {
+		printf("no data...\n");
+		return;
+	}
 	data *temp = g_head;
 	printf("===================================================\n");
 	while(temp->next) {
@@ -20,8 +40,7 @@ void printAll() {
 		printf("name : %s\n",temp->name);	
 		printf("===================================================\n");
 		temp = temp->next;	
-	}	
-	printf("===================================================\n");
+	}
 	printf("num : %d\n",temp->num);	
 	printf("name : %s\n",temp->name);	
 	printf("===================================================\n");	
@@ -37,7 +56,7 @@ int insert(int num, char * name) {
 	if(name != NULL) {
 		strcpy(node->name, name);
 	}
-
+	
 	if(g_head == NULL) {
 		g_head = node;
 		g_tail = node;
@@ -54,7 +73,51 @@ int insert(int num, char * name) {
 	return 0;
 }
 
+data *pop() {
+	if(g_head == NULL){
+		return NULL;
+	}
+	data *node = g_head;
 
+	if(g_head->next != NULL){
+		g_head = g_head->next;
+	}else{
+		g_head = NULL;
+		g_tail = NULL;
+	}
+	
+	return node;
+
+}
+
+data *stack_pop() {
+	
+	if(g_head == NULL) {
+		return NULL;
+	}
+	
+	data *node = g_tail; 
+
+	data *temp = g_head;
+	data *before = NULL;
+
+	while(temp->next) {
+		before = temp;
+		temp = temp->next;
+	}
+	
+	g_tail = before;
+	if(before != NULL) {
+		before->next = NULL;
+	}
+
+	if(g_tail == NULL) {
+		g_head = NULL;
+	}
+
+	return node;
+
+}
 
 int main() {
 	char name[20] = "";
@@ -63,10 +126,31 @@ int main() {
 		insert(i,name);
 	}	
 	printAll();
-	
 
+	/*for(int i = 0; i < 10; i++) {
+	data * node1 = stack_pop();
+	printf("pop num : %d\n",node1 -> num);
+	printf("pop name : %s\n",node1 -> name);
+	free(node1);
+	}
+	printAll();*/
 
+	for(int i = 0; i < 10; i++) {
+	data * node1 = pop();
+	printf("pop num : %d\n",node1 -> num);
+	printf("pop name : %s\n",node1 -> name);
+	free(node1);
+	}
+	printAll();
 
+	/*data *node = find(7);
+	if(node != NULL){
+		printf("find num : %d\n",node -> num);
+		printf("find name : %s\n",node -> name);
+
+	}else{
+		printf("error\n");
+	}*/
 
 	return 0;
 }
